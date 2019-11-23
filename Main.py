2377cd9f9254f2a796cedcf6f9afabafd8c07b2f
gemeln <mtang317@gmail.com>
2019-11-23 09:28:56 -0600

    loop

--- Main.py
+++ Main.py
@@ -1,14 +1,25 @@
 from nes_py.wrappers import JoypadSpace
 import gym_super_mario_bros
+import time
 from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
+
 env = gym_super_mario_bros.make('SuperMarioBros-v0')
 env = JoypadSpace(env, SIMPLE_MOVEMENT)
 
-done = True
-for step in range(5000):
-    if done:
-        state = env.reset()
-    state, reward, done, info = env.step(env.action_space.sample())
-    env.render()
+# Number of runs before danger network is updated
+danger_learn_rate = 100;
 
-env.close()
+# Do many runs
+for run in range(5000):
+    if run % danger_learn_rate == 0:
+        print("Update here")
+    # Step through run
+    done = True
+    for step in range(5000):
+        if done:
+            state = env.reset()
+        time.sleep(0.1);
+        state, reward, done, info = env.step(env.action_space.sample())
+        print(env.action_space);
+        env.render()
+    env.close()
