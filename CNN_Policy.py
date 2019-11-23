1c7837246db42d1cda434d0cff739fe9bdeae988
Alex <Sunny.hsia1785@gmail.com>
2019-11-23 23:26:21 +0800

    DQN

--- CNN_Policy.py
+++ CNN_Policy.py
@@ -4,6 +4,7 @@ import torch.optim as optim
 import torch.nn.functional as F
 import numpy as np
 from torch.distributions import Normal
+import random
 import math
 import time
 import os
@@ -20,15 +21,29 @@ class CNN_Policy(nn.Module):
         self.bn3 = nn.BatchNorm2d(32)
         self.linear = nn.Linear(950400, output_dim)
 
-
-
-    def forward(self, x): # Outputs mean of Gaussian distribution
+    def forward(self, x):
         x = F.relu(self.bn1(self.conv1(x)))
         x = F.relu(self.bn2(self.conv2(x)))
         x = F.relu(self.bn3(self.conv3(x)))
         x = x.view(x.size(0), -1)
         return F.relu(self.linear(x))
 
+
+def select_action(state, initial_epsilon, final_epsilon, steps_done, epsilon_decay):
+    sample = random.random()
+    eps_threshold = final_epsilon + (initial_epsilon - final_epsilon) * \
+                    math.exp(-1. * steps_done / epsilon_decay)
+    if sample > eps_threshold:
+        with torch.no_grad():
+            state = torch.Tensor(state)
+            steps_done += 1
+            q_calc = model(state)
+            node_activated = int(torch.argmax(q_calc))
+            return node_activated
+    else:
+        node_activated = random.randint(0,1)
+        steps_done += 1
+        return node_activated
 
 disc = 0.99
 clip_f = 0.2
